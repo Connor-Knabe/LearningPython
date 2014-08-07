b5be30d9834bb1202c2340c2fdb521199ed56a42
Connor Knabe <connorknabe@gmail.com>
2014-08-07 12:44:32 -0500

    Reversed array

---
+++ Testing/reverseArray.py
@@ -0,0 +1,11 @@
+array = [5,10,15,20]
+#array2 = [1,2,3,4]
+
+
+for i in range (0, len(array)):
+	print "Array normal: ", array[i]
+
+array.reverse()
+
+for i in range (0, len(array)):
+	print "Array reversed: ", array[i]
