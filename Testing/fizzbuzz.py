4c8fe38d8ebcc3dcfc30321977a0acda4137b2bf
Connor Knabe <connorknabe@gmail.com>
2014-08-07 13:22:22 -0500

    Almost finished with problem 4 fizzbuzz

--- Testing/fizzbuzz.py
+++ Testing/fizzbuzz.py
@@ -1,11 +1,22 @@
-fiz = "fizzbuzz"
+fiz = "fizzbuzzb"
 
 f = -1
 
 #f = fiz.rfind(f)
 
 f = fiz.find("f")
+b = fiz.rfind("b")
 
 count = fiz.count("z")
 
 print fiz, "count is ", count
+
+print "Was f found?", f
+
+
+if f == 0:
+	print "fizz"
+if b == len(fiz)-1:
+	print "buzz"
+if f == 0 and b == len(fiz)-1:
+	print "fizzbuzz"
