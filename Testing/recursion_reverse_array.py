cc5f2bd5ac4a5b7896b5a2effb322f82c1c9152b
Connor Knabe <connorknabe@gmail.com>
2014-08-11 15:56:25 -0700

    More changes

--- Testing/recursion_reverse_array.py
+++ Testing/recursion_reverse_array.py
@@ -1,8 +1,9 @@
-arr = [1,2]
+arr = [1,2,3]
 
 def helper(arr,i,k,rev_arr):
 	arr_size_index = len(arr)-1
 	print "i is ",i
+	print "k is ",k
 
 	for d in range (len(rev_arr)):
 		print rev_arr[d] 
@@ -16,16 +17,22 @@ def helper(arr,i,k,rev_arr):
 	if k < len(rev_arr):
 		helper(arr,i,k,rev_arr)
 	else:
+		print "DONE"
+		for d in range (len(rev_arr)):
+			print rev_arr[d] 
 		return rev_arr
 
 def reverse(arr):
 	#Your code goes here and remember you can add other methods to help out if you like.
 	rev_arr = arr
+	sol_arr = []
 
 	sol_arr = helper(arr,len(arr)-1,0,rev_arr)
 
-	for i in range (len(sol_arr))
-		print sol_arr[i]
+	#print sol_arr[0]
+
+	#for i in range (len(sol_arr)):
+	#	print sol_arr[i]
 
 	
 reverse(arr)
