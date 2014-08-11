fad8851592dd0a5889ff8e157170ef804fd227f1
Connor Knabe <connorknabe@gmail.com>
2014-08-11 16:13:05 -0700

    Almost finished just need to copy array instead of pointer to it

--- Testing/recursion_reverse_array.py
+++ Testing/recursion_reverse_array.py
@@ -1,4 +1,4 @@
-arr = [1,2,3]
+arr = [1,2,3,4,5,6,7,8]
 
 def helper(arr,i,k,rev_arr):
 	arr_size_index = len(arr)-1
@@ -7,6 +7,8 @@ def helper(arr,i,k,rev_arr):
 
 	print "rev_arr[k] = ", rev_arr[k]
 	print "arr[i] = ", arr[i]
+
+	print "\nArry[0]", arr[0]
 
 	rev_arr[k] = arr[i]
 
@@ -31,7 +33,8 @@ def helper(arr,i,k,rev_arr):
 
 def reverse(arr):
 	#Your code goes here and remember you can add other methods to help out if you like.
-	rev_arr = arr
+	rev_arr = [1,2,3,4,5,6,7,8]
+
 	sol_arr = []
 
 	sol_arr = helper(arr,len(arr)-1,0,rev_arr)
