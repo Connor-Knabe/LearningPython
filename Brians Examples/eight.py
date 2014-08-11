fd2e040712e7398afc5cd8cf86bbf3cf7a8bb549
Connor Knabe <connorknabe@gmail.com>
2014-08-11 16:40:24 -0700

    Working on problem 8

--- Brians Examples/eight.py
+++ Brians Examples/eight.py
@@ -9,7 +9,16 @@
 
 def caught_speeding(speed, is_birthday):
 	#Your code goes here
-	if speed <= 60:
+	if speed <= 60 and not is_birthday:
 		return 0 
-	elif speed > 60 and <= 80 and !is_birthday
+	elif speed > 60 and speed <= 80 and not is_birthday:
 		return 1
+	elif speed > 80 and not is_birthday:
+		return 2
+	elif speed <= 65 and is_birthday:
+		return 0
+	elif speed > 65 and speed <= 85 and not is_birthday:
+		return 1
+	elif speed > 85 and is_birthday:
+		return 2
+
--- Testing/speeding.py
+++ Testing/speeding.py
@@ -4,6 +4,16 @@ speed = 60
 
 
 if speed <= 60:
+	print "not speed"
+elif speed > 60 and speed <= 80 and not is_birthday:
+	print "speed"
+
+
+if speed <= 60:
 	return 0 
-elif speed > 60 and speed <= 80 and is_birthday
+elif speed > 60 and speed <= 80:
 	return 1
+elif speed > 80:
+	return 2
+
+
