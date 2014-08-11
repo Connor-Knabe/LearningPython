b6535192074835909ef7cc1c56fc087adabf3753
Connor Knabe <connorknabe@gmail.com>
2014-08-11 16:33:14 -0700

    working on the not operator

---
+++ Testing/cigars.py
@@ -0,0 +1,6 @@
+cigars = 40
+
+if cigars >= 40 and cigars < 60:
+	print "TRUE"
+else:
+	print "FALSE"
---
+++ Testing/speeding.py
@@ -0,0 +1,9 @@
+is_birthday = True
+
+speed = 60
+
+
+if speed <= 60:
+	return 0 
+elif speed > 60 and speed <= 80 and is_birthday
+	return 1
