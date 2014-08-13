6511e95771654ba138d4574369aaae29c6c89c41
Connor Knabe <connorknabe@gmail.com>
2014-08-12 20:53:09 -0500

    corrected problem 5

--- Brians Examples/five.py
+++ Brians Examples/five.py
@@ -1,24 +1,29 @@
 #arr = [0,1,2,3,4,22]
 
 def helper(sol_arr, arr, i):
-	print "length=", len(arr), "\n"
+	#print "length=", len(arr), "\n"
 
-	print "Index", i, arr[i]
+	#print "Index", i, arr[i]
 	arr[i] *= 2
+
+	#print "Index after", i, arr[i]
 	i += 1
+
 
 	if i < len(arr):
 
 		#print "Got into if && index = ", i
-		helper(sol_arr,arr,i)
+		return helper(sol_arr,arr,i)
 	else:
 		#print "Returning from if", i
 		return arr
 
 def times2(arr):
+	print arr
 	sol_arr = []
-	solution_arr = helper(sol_arr, arr, 0)
 
+	solution_arr = helper(sol_arr, arr, 0)
+	print solution_arr
 	return solution_arr
 
 #times2(arr)
--- Testing/recursion_reverse_array.py
+++ Testing/recursion_reverse_array.py
@@ -1,43 +1,43 @@
-arr = [1,2,3,4,5,6,7,8]
 
-def helper(arr,i,k,rev_arr):
-	arr_size_index = len(arr)-1
-	print "i is ",i
-	print "k is ",k
+def helper(arr,i,rev_arr):
+	#print "i is ",i
+	#print "k is ",k
 
-	print "rev_arr[k] = ", rev_arr[k]
-	print "arr[i] = ", arr[i]
+	#print "rev_arr[k] = ", rev_arr[k]
+	#print "arr[i] = ", arr[i]
 
-	print "\nArry[0]", arr[0]
+	#print "\nArry[0]", arr[0]
 
-	rev_arr[k] = arr[i]
+	rev_arr.append(arr[i])
 
-	i -= 1
-	k += 1
+	#k += 1
 
-	if k >= len(rev_arr):
-		print "DONE"
-		for d in range (len(rev_arr)):
-			print rev_arr[d] 
+	#if k >= len(rev_arr):
+	#	print "DONE"
+	#	#for d in range (len(rev_arr)):
+	#	#	print rev_arr[d] 
+	#	return rev_arr
+	#else:
+	#	return helper(arr,i,k,rev_arr)
+
+	if i == 0:
 		return rev_arr
 	else:
-		helper(arr,i,k,rev_arr)
+		i -= 1
 
-	#if k < len(rev_arr):
-	#	helper(arr,i,k,rev_arr)
-	#else:
-	#	print "DONE"
-	#	for d in range (len(rev_arr)):
-	#		print rev_arr[d] 
-	#	return rev_arr
+		return helper(arr,i,rev_arr)
+
 
 def reverse(arr):
 	#Your code goes here and remember you can add other methods to help out if you like.
-	rev_arr = [1,2,3,4,5,6,7,8]
+	
+	arr2 = [1,2,3,4,5,6,7,8]
 
-	sol_arr = []
+	#sol_arr = []
 
-	sol_arr = helper(arr,len(arr)-1,0,rev_arr)
+	sol_arr = helper(arr2,len(arr2)-1,[])
+
+	print sol_arr
 
 	#print sol_arr[0]
 
@@ -45,4 +45,4 @@ def reverse(arr):
 	#	print sol_arr[i]
 
 	
-reverse(arr)
+reverse([])
