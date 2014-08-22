eabcae9008077e2f38cb3a5a9ac5e4b7f309cd56
Connor Knabe <connorknabe@gmail.com>
2014-08-21 19:28:34 -0500

    Refactored to a one liner

--- Codingbat/monkey_trouble.py
+++ Codingbat/monkey_trouble.py
@@ -1,7 +1,2 @@
 def monkey_trouble(a_smile, b_smile):
-	if a_smile and b_smile:
-		return True
-	elif not a_smile and not b_smile:
-		return True
-	else:
-		return False
+	return ((a_smile and b_smile) or (not a_smile and not b_smile))
