e0780a795c47440e205643813590a430bb4d4bde
Connor Knabe <connorknabe@gmail.com>
2014-08-24 20:58:23 -0500

    One liner solution from CB

--- Codingbat/parrot_trouble.py
+++ Codingbat/parrot_trouble.py
@@ -1,7 +1,2 @@
 def parrot_trouble(talking, hour):
-	if talking and hour < 7:
-		return True
-	elif talking and hour > 20:
-		return True
-	else:
-		return False
+	return (talking and (hour < 7 or hour > 20))
