32d78e2f53fc59a531905aba73d1cf566a8b5862
Connor Knabe <connorknabe@gmail.com>
2014-08-25 15:24:26 -0500

    pos_neg works

---
+++ Codingbat/pos_neg.py
@@ -0,0 +1,8 @@
+def pos_neg(a, b, negative):
+  
+    if (negative):
+        if (a < 0 and b < 0):
+	    return True
+	else:
+	    return False
+    return (a > 0 and b < 0) or (b > 0 and a < 0) and not negative
