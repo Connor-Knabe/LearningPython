908a52c6687fe2619b9610b60530fd67ff1081b4
Connor Knabe <connorknabe@gmail.com>
2014-08-25 15:39:09 -0500

    Works on coding bat

--- Codingbat/not_string.py
+++ Codingbat/not_string.py
@@ -1,17 +1,5 @@
-#def not_string(str):
-
-str = "not string"
-
-
-if (str[0:3] == "not"):
-	print str
-else:
-	print ("not " + str)
-
-str = "string"
-if (str[0:3] == "not"):
-	print str
-else:
-	print str
-
-	print ("not " + str)
+def not_string(str):
+    if (str[0:3] == "not"):
+ 		return str
+    else:
+		return ("not " + str)
