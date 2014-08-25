13cf0f28e5aad3085bf218fba1ad3399b340c823
Connor Knabe <connorknabe@gmail.com>
2014-08-24 21:39:54 -0500

    Works forgot a paren

--- Codingbat/near_hundred.py
+++ Codingbat/near_hundred.py
@@ -1,7 +1,7 @@
 def near_hundred(n):
-	if (n < 111) and abs(n-100 < 11):
-		return True
-	elif (n > 111) and abs(n-200 < 11):
-		return True
-	else:
-		return False
+    if (abs(n-100) < 11):
+        return True
+    elif (abs(n-200) < 11):
+        return True
+    else:
+        return False
