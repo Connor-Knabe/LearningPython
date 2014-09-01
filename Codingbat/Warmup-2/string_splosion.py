d4b422f2046d595fd5b98bb84ec901ffaedbf8e4
Connor Knabe <connorknabe@gmail.com>
2014-08-31 22:26:07 -0500

    Works with a for loop but only for up to 4 characters

--- Codingbat/Warmup-2/string_splosion.py
+++ Codingbat/Warmup-2/string_splosion.py
@@ -1,14 +1,40 @@
-string = "abc"
+
+#finalString += string[:1]
+#finalString += string[:1]
+
+#finalString += string[1:2]
+#finalString += string[:1]
+#finalString += string[1:2]
+#finalString += string[2:3]
+#finalString += string[0:len(string)]
+
+#for i in range (0,len(string)):
+#	print i
+#	if i < 1:
+#		finalString += string[:1]
+#		finalString += string[:1]
+#	elif i < 2:
+#		finalString += string[1:2]
+#		finalString += string[:1]
+#	elif i < 3:
+#		finalString += string[:len(string)]
+
+#print finalString
+
+
+string = "Code"
 
 finalString = ""
 
-finalString += string[:1]
-finalString += string[:1]
+finalString += string[0:1] #first letter
+finalString += string[0:1] #first letter
 
-finalString += string[1:2]
-finalString += string[:1]
-finalString += string[1:2]
-finalString += string[2:3]
-finalString += string[0:len(string)]
+finalString += string[1:2] #second letter
+
+finalString += string[0:3] #third letter
+
+finalString += string [0:4] #fourth letter
+
+
 
 print finalString
