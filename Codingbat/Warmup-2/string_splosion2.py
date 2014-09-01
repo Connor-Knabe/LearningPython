aa98d5b8e696d2ad79ce039aa6f236dda2b30567
Connor Knabe <connorknabe@gmail.com>
2014-09-01 08:06:37 -0500

    Fails for one test that needs to be taken into account

--- Codingbat/Warmup-2/string_splosion2.py
+++ Codingbat/Warmup-2/string_splosion2.py
@@ -1,22 +1,21 @@
-string = "Kitten"
 finalString = ""
+str = "x"
 
 
-
-for i in range (len(string)):
-	if i == 0 and len(string) > 0:
-		finalString += string[0]
-		finalString += string[0]
+for i in range (len(str)):
+	if i == 0 and len(str) > 0:
+		finalString += str[0]
+		finalString += str[0]
 	elif i == 1:
-		finalString += string[1]
+		finalString += str[1]
 	elif i == 2:
-		finalString += string[0:3]
-	elif i > 2 and len(string)-1 != i: 
+		finalString += str[0:3]
+	elif i > 2 and len(str)-1 != i: 
 		print "HE"
-		finalString += string[0:i+1]
-	elif i == len(string)-1:
+		finalString += str[0:i+1]
+	elif i == len(str)-1:
 		print "HI"
-		finalString += string
+		finalString += str
 
 
 print finalString
