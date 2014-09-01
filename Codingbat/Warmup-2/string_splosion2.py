0caf314e69e6c3a18f149a8b5d8aec0727351fdf
Connor Knabe <connorknabe@gmail.com>
2014-09-01 08:08:16 -0500

    Works for all tests

--- Codingbat/Warmup-2/string_splosion2.py
+++ Codingbat/Warmup-2/string_splosion2.py
@@ -1,22 +1,23 @@
-finalString = ""
-str = "x"
+def string_splosion(str):
+    finalString = ""
 
 
-for i in range (len(str)):
-	if i == 0 and len(str) > 0:
-		finalString += str[0]
-		finalString += str[0]
+
+    for i in range (len(str)):
+        if i == 0 and len(str) == 1:
+            return str
+        if i == 0 and len(str) > 0:
+	    finalString += str[0]
+	    finalString += str[0]
 	elif i == 1:
-		finalString += str[1]
+	    finalString += str[1]
 	elif i == 2:
-		finalString += str[0:3]
+	    finalString += str[0:3]
 	elif i > 2 and len(str)-1 != i: 
-		print "HE"
-		finalString += str[0:i+1]
+	    finalString += str[0:i+1]
 	elif i == len(str)-1:
-		print "HI"
-		finalString += str
+	    finalString += str
 
 
-print finalString
+    return finalString
 
