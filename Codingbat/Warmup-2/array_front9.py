1cfc62a49943da29d97d630b3d2cb10d8c8c1038
Connor Knabe <connorknabe@gmail.com>
2014-08-31 21:10:40 -0500

    Need to convert to codingbat

--- Codingbat/Warmup-2/array_front9.py
+++ Codingbat/Warmup-2/array_front9.py
@@ -1,4 +1,4 @@
-nums = [1,2,1,1,9,4,1,9]
+nums = [9,2,1,1,9,1,1,9]
 
 print len(nums)
 k = 0
@@ -6,7 +6,7 @@ k = 0
 
 
 for i in range (0,len(nums)):
-	if nums[i] == 9 and k <= 4:
+	if nums[i] == 9 and k < 4:
 		print "true"
 	k += 1
 print "false"
