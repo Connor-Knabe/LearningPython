5a104ea20f120c5e8ef101903bd9e3ea7b6d614e
Connor Knabe <connorknabe@gmail.com>
2014-08-31 21:08:45 -0500

    Very close seems to be working just a few more tests

--- Codingbat/Warmup-2/array_front9.py
+++ Codingbat/Warmup-2/array_front9.py
@@ -1,6 +1,12 @@
-arr = [9,1,3,1,9]
+nums = [1,2,1,1,9,4,1,9]
+
+print len(nums)
+k = 0
 
 
-for i in range (0,4):
-	if arr[i]==9:
-		print "TRUE"
+
+for i in range (0,len(nums)):
+	if nums[i] == 9 and k <= 4:
+		print "true"
+	k += 1
+print "false"
