ac6c7491b42b62d235ab19cb03962c3f008feff4
Connor Knabe <connorknabe@gmail.com>
2014-09-01 07:43:12 -0500

    Getting closer

--- Codingbat/Warmup-2/string_splosion2.py
+++ Codingbat/Warmup-2/string_splosion2.py
@@ -1,18 +1,26 @@
-def string_splosion(str):
-  
-    string = str
-    finalString = ""
+string = "There"
+finalString = ""
 
-    for i in range(0,len(string)):
-        if i == 0:
-	    finalString += string[0]
-	    finalString += string[0]
-	elif i == 1:
-	    finalString += string[1]
-	elif i == 2:
-	     finalString += string[0:3]
-	elif i == 3:
-	    finalString += string[0:4]
 
-    return finalString
+
+for i in range(0,len(string)):
+	if i == 0:
+		finalString += string[0]
+		finalString += string[0]
+	elif i == 1 and len(string) > 1:
+		finalString += string[1]
+	elif i == 2 and len(string) > 2:
+		finalString += string[0:3]
+	elif i == 3 and len(string) > 3:
+		finalString += string[0:4]
+	elif i == 4 and len(string) > 4:
+		finalString += string[0:len(string)-1]
+	elif i == len(string)-2:
+		finalString += string
+
+
+
+
+
+print finalString
 
