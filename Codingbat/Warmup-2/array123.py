421b4b0dfb7a1ab136156f693a18a6cbd4348d5d
Connor Knabe <connorknabe@gmail.com>
2014-08-31 21:39:47 -0500

    Codingbats solution

--- Codingbat/Warmup-2/array123.py
+++ Codingbat/Warmup-2/array123.py
@@ -1,17 +1,5 @@
 def array123(nums):
-  
-    one = 0
-    two = 0
-    three = 0
-
-    for i in range (0,len(nums)):
-        if nums[i] == 1:
-	    one += 1
-	elif nums[i] == 2:
-	    two += 1
-	elif nums[i] == 3:
-	    three += 1
-    if one >= 1 and two >= 1 and three >= 1:
-        return True
-    else:
-        return False
+  for i in range(len(nums)-2):
+    if nums[i]==1 and nums[i+1]==2 and nums[i+2]==3:
+      return True
+  return False
