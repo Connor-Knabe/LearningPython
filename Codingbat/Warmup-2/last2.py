ae40155f2df50b6c9d6cc62fd638fe2c79866a45
Connor Knabe <connorknabe@gmail.com>
2014-09-01 20:36:43 -0500

    Will come back to this one

--- Codingbat/Warmup-2/last2.py
+++ Codingbat/Warmup-2/last2.py
@@ -1,5 +1,13 @@
-str = "hihi"
+str = "hixxhi"
 firstSubStr = ""
+secondSubStr = ""
+j = 0
 for i in range (len(str)):
 	firstSubStr = str[i:i+2]
-	print firstSubStr
+	for k in range (len(str)):
+		secondSubStr = str[k+1:k+3]
+		if firstSubStr == secondSubStr:
+			j += 1
+		print "first",firstSubStr, "second", secondSubStr
+
+print j
