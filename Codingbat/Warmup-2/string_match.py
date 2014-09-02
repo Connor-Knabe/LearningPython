807b757d2ed2ca654e22eec1c72b6b0369e8af6c
Connor Knabe <connorknabe@gmail.com>
2014-09-01 20:48:18 -0500

    Almost there

--- Codingbat/Warmup-2/string_match.py
+++ Codingbat/Warmup-2/string_match.py
@@ -1,6 +1,13 @@
 string1 = "abc"
 string2 = "abc"
 
+sub1 = ""
+sub2 = ""
+
 for i in range (len(string1)):
-	if string1[i:i+2] == string2[i:i+2]:
+	sub1 = string1[i:i+2]
+	sub2 = string2[i:i+2]
+
+	if sub1 == sub2 and len(sub1) > 1 and len(sub2) > 1:
+		print "STRING 1",string1[i:i+2]
 		print "YE"
