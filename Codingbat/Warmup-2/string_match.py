1f827b5f5f10cfde9e5627eb283e9751649ca699
Connor Knabe <connorknabe@gmail.com>
2014-09-01 20:50:38 -0500

    Works for all tests

--- Codingbat/Warmup-2/string_match.py
+++ Codingbat/Warmup-2/string_match.py
@@ -1,13 +1,16 @@
-string1 = "abc"
-string2 = "abc"
+def string_match(a, b):
+  
+    string1 = a
+    string2 = b
 
-sub1 = ""
-sub2 = ""
+    sub1 = ""
+    sub2 = ""
+    subStrNum = 0
 
-for i in range (len(string1)):
+    for i in range (len(string1)):
 	sub1 = string1[i:i+2]
 	sub2 = string2[i:i+2]
 
 	if sub1 == sub2 and len(sub1) > 1 and len(sub2) > 1:
-		print "STRING 1",string1[i:i+2]
-		print "YE"
+		subStrNum += 1
+    return subStrNum
