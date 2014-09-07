6b9bcc1b00045473b0ddf32cafd3ebea215278fc
Connor Knabe <connorknabe@gmail.com>
2014-09-07 10:16:11 -0500

    All tests pass codingbat, added a check to make sure the string is at end of line

--- Codingbat/String-2/end_other.py
+++ Codingbat/String-2/end_other.py
@@ -1,17 +1,20 @@
-a = "AbC"
-b = "HiaBc"
+def end_other(a, b):
+  
+    a = a.lower()
+    b = b.lower()  
+    theLen = 0
 
-a = a.lower()
-b = b.lower()
-print a
+    if len(a) > len(b):
+        theLen = len(a) - len(b)
 
-if len(a) > len(b):
-    for i in range(len(a)):
-    	if a[i:i+len(b)] == b and i < len(a):
-    		print "true"
+        for i in range(len(a)):
+    	    if a[i:i+len(b)] == b and i >= theLen:
+    		return True
 
-else:
-    for i in range(len(b)):
-    	if b[i:i+len(a)] == a and i < len(b):
-    		print "true"
-print "false"
+    else:
+        theLen = len(b) - len(a)
+
+        for i in range(len(b)):
+    	    if b[i:i+len(a)] == a and i >= theLen:
+    		return True
+    return False
