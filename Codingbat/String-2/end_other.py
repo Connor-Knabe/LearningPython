f1388e68f6650f3f8ce0b48c3505fd496d5cd98d
Connor Knabe <connorknabe@gmail.com>
2014-09-07 10:07:26 -0500

    Still working on end_other problem

--- Codingbat/String-2/end_other.py
+++ Codingbat/String-2/end_other.py
@@ -1,13 +1,17 @@
-a = "Hiabc"
-b = "abc"
+a = "AbC"
+b = "HiaBc"
+
+a = a.lower()
+b = b.lower()
+print a
 
 if len(a) > len(b):
     for i in range(len(a)):
-    	if a[i:i+len(b)] == b:
-    		return true
+    	if a[i:i+len(b)] == b and i < len(a):
+    		print "true"
 
 else:
     for i in range(len(b)):
-    	if b[i:i+len(a)] == a:
-    		return true
-return false
+    	if b[i:i+len(a)] == a and i < len(b):
+    		print "true"
+print "false"
