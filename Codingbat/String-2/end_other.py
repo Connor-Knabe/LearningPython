68eb20f6cb086e073a3e1c7e61c7d47da697676e
Connor Knabe <connorknabe@gmail.com>
2014-09-07 10:17:06 -0500

    Codingbats solution

--- Codingbat/String-2/end_other.py
+++ Codingbat/String-2/end_other.py
@@ -1,20 +1,4 @@
 def end_other(a, b):
-  
-    a = a.lower()
-    b = b.lower()  
-    theLen = 0
-
-    if len(a) > len(b):
-        theLen = len(a) - len(b)
-
-        for i in range(len(a)):
-    	    if a[i:i+len(b)] == b and i >= theLen:
-    		return True
-
-    else:
-        theLen = len(b) - len(a)
-
-        for i in range(len(b)):
-    	    if b[i:i+len(a)] == a and i >= theLen:
-    		return True
-    return False
+  a = a.lower()
+  b = b.lower()
+  return (b.endswith(a) or a.endswith(b))
