5a4b6b73d2101e904939b6b4dccd0ed8858eb2da
Connor Knabe <connorknabe@gmail.com>
2014-09-06 22:05:48 -0500

    Working just need to change formatting

---
+++ Codingbat/String-2/cat_dog.py
@@ -0,0 +1,12 @@
+str = "catcat"
+catCount = 0 
+dogCount = 0
+
+for i in range (len(str)):
+	if str[i:i+3] == "cat":
+		catCount += 1
+	if str[i:i+3] == "dog":
+		dogCount += 1
+
+print "cat", catCount
+print "dog", dogCount
