24331f63bc87a7c1dd88cb009e6ce1bb6e31a0c2
Connor Knabe <connorknabe@gmail.com>
2014-09-06 22:08:05 -0500

    All codingbat tests pass

--- Codingbat/String-2/cat_dog.py
+++ Codingbat/String-2/cat_dog.py
@@ -1,12 +1,12 @@
-str = "catcat"
-catCount = 0 
-dogCount = 0
+def cat_dog(str):
+    catCount = 0 
+    dogCount = 0
 
-for i in range (len(str)):
-	if str[i:i+3] == "cat":
-		catCount += 1
+    for i in range (len(str)):
+        if str[i:i+3] == "cat":
+	    catCount += 1
 	if str[i:i+3] == "dog":
-		dogCount += 1
+	    dogCount += 1
 
-print "cat", catCount
-print "dog", dogCount
+    return catCount == dogCount
+
