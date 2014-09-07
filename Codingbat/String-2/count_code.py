d95e31c2a2ed5e5938e0b4f4fc2491bf0b6188f3
Connor Knabe <connorknabe@gmail.com>
2014-09-07 09:44:21 -0500

    All tests pass on codingbat

--- Codingbat/String-2/count_code.py
+++ Codingbat/String-2/count_code.py
@@ -1,11 +1,14 @@
-str = "aaacodxbbb"
-finalStr = ""
-for i in range (len(str)):
-	finalStr = str[i:i+4]
-	if finalStr[:2] == "co":
-		print "YES"
-	if finalStr[3:] == "e":
-		print "YES"
-	print finalStr
-	#if str[i:i+4] == "code":
-		#print "YES"
+def count_code(str):
+  
+    finalStr = ""
+
+    count = 0
+
+    for i in range (len(str)):
+        finalStr = str[i:i+4]
+
+	if finalStr[:2] == "co" and finalStr[3:] == "e":
+	    count += 1
+
+    return count
+	
