b16e3159b9922588062456e2545529483e340b3f
Connor Knabe <connorknabe@gmail.com>
2014-09-09 17:00:00 -0500

    All tests pass on codingbat

--- Codingbat/List-1/same_first_last.py
+++ Codingbat/List-1/same_first_last.py
@@ -1,7 +1,9 @@
-    nums = [1,2,3]
+def same_first_last(nums):
+  
 
-    if ( len(nums) > 1 and nums[0]==nums[-1]):
-	    print "TRUE"
+    if ( len(nums) > 0 and nums[0]==nums[-1]):
+        return True
     else:
     	return False
+
 
