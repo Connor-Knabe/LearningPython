fcb812bf7257b34c4ec8fb65e4681c9b788496c7
Connor Knabe <connorknabe@gmail.com>
2014-09-09 17:00:28 -0500

    One liner solution

--- Codingbat/List-1/same_first_last.py
+++ Codingbat/List-1/same_first_last.py
@@ -1,9 +1,4 @@
 def same_first_last(nums):
   
 
-    if ( len(nums) > 0 and nums[0]==nums[-1]):
-        return True
-    else:
-    	return False
-
-
+    return ( len(nums) > 0 and nums[0]==nums[-1])
