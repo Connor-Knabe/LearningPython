fdfbcb4cf8e25553509416b6c7d007053cbd686a
Connor Knabe <connorknabe@gmail.com>
2014-09-13 13:27:07 -0500

    Works

--- Dynafroms Example/1-100.py
+++ Dynafroms Example/1-100.py
@@ -1,12 +1,17 @@
+#This problem demonstrates a simple way to solve the problem if someone is given an array in any order numbers 1-100 it will return that missing number
+
 arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
 expectedSum = 0
+newArraySum = 0
 
 for i in range (len(arr)):
 	expectedSum += arr[i]
-print "SUM",sum
+print "Expected sum of array containing numbers 1-100 is:",expectedSum
 
-curArraySum = 0
+arr[1]=0
+
 
 for i in range (len(arr)):
-	curArraySum += arr[i]
+	newArraySum += arr[i]
 
+print "The missing number is", expectedSum - newArraySum
