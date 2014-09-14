462f03a53ef6e93ec02522221977f80db5d11071
Connor Knabe <connorknabe@gmail.com>
2014-09-14 13:31:46 -0500

    Almost working but some cases expect lower numbers

--- Codingbat/List-1/max_end3.py
+++ Codingbat/List-1/max_end3.py
@@ -1,11 +1,10 @@
-nums = [1,2,3]
-max = 0
-for i in range (len(nums)):
-	if (nums[i] > max):
-		max = nums[i]
+    max = 0
+    for i in range (len(nums)):
+    	if (nums[i] > max):
+    		max = nums[i]
 
-for i in range (len(nums)):
-	nums[i]=max
+    for i in range (len(nums)):
+    	nums[i]=max
 
-print nums
+    return nums
 
