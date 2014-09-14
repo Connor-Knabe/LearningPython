bac959320ef693dff42f4a8860fbbb8fb7b6d114
Connor Knabe <connorknabe@gmail.com>
2014-09-14 15:08:37 -0500

    Codingbat format

--- Codingbat/List-1/sum2.py
+++ Codingbat/List-1/sum2.py
@@ -1,7 +1,8 @@
-nums = [2,3]
-sum=0
-if len(nums)<2:
-    sum = 0 if len(nums)==0 else nums[0]
-else:
-	sum = nums[0] + nums[1]
-print sum
+def sum2(nums):
+  
+    sum=0
+    if len(nums)<2:
+        sum = 0 if len(nums)==0 else nums[0]
+    else:
+    	sum = nums[0] + nums[1]
+    return sum
