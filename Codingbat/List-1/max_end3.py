f8a49baf2311409a1bf34580df5525f1acaa3777
Connor Knabe <connorknabe@gmail.com>
2014-09-14 13:28:11 -0500

    ALmost there

--- Codingbat/List-1/max_end3.py
+++ Codingbat/List-1/max_end3.py
@@ -1,7 +1,10 @@
-    nums = [1,2,3]
-    max = 0
-    for i in range (len(nums)):
-    	if (nums[i] > max):
-    		max == nums[i]
+nums = [1,2,3]
+max = 0
+for i in range (len(nums)):
+	if (nums[i] > max):
+		max = nums[i]
+
+for i in range (len(nums)):
+	nums[i]=max
 
 print max
