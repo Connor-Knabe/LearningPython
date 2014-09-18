0208998050ffa235ee1ee4303179378292499e90
Connor Knabe <connorknabe@gmail.com>
2014-09-18 18:26:31 -0500

    Refactored even more removing white space

--- PythonGradingTools/print.py
+++ PythonGradingTools/print.py
@@ -1,11 +1,8 @@
 import os
-str = ""
-out1 = "" 
 arr = []
 outFile = open('./names.txt', 'w+')
 i = 0
 for filenames in os.listdir('.'):
-    #str = filenames.split(".")[0]
     arr.append(filenames.split(".")[0])
     i += 1
 
