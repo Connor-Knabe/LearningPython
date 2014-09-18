7a6f018262161e98f8c9cdac1b78ca0e381cf275
Connor Knabe <connorknabe@gmail.com>
2014-09-18 18:39:37 -0500

    Making the rename script more effective

--- PythonGradingTools/rename.py
+++ PythonGradingTools/rename.py
@@ -1,7 +1,6 @@
 import os
-lab = ""
 for filename in os.listdir("."):
 	if filename.startswith("Lab "):
-		lab = filename.split(" ")
-		os.rename(filename, filename[6:12])
-print lab
+		filename = filename.split("_")[1]
+		print filename
+		#os.rename(filename, filename[6:12])
