c2beddd95a2ac6c2051562711549db06f856ab87
Connor Knabe <connorknabe@gmail.com>
2014-09-18 18:52:46 -0500

    Removed unnecessary code from print script

--- PythonGradingTools/print.py
+++ PythonGradingTools/print.py
@@ -1,10 +1,8 @@
 import os
 arr = []
 outFile = open('./names.txt', 'w+')
-i = 0
 for filenames in os.listdir('.'):
     arr.append(filenames.split(".")[0])
-    i += 1
 arr.sort()
 for j in range (len(arr)):
     outFile.write(arr[j] + " -" + "\n")
