d38210e2610174c384ed8940fc5edbb47380c43f
Connor Knabe <connorknabe@gmail.com>
2014-09-18 19:00:46 -0500

    Added documentation

--- PythonGradingTools/renamePrint.py
+++ PythonGradingTools/renamePrint.py
@@ -1,13 +1,18 @@
 import os
-outFile = open('./cs3380Grades.txt', 'w+')
+outFile = open('./Grades.txt', 'w+')
 pwprtName = ""
 pwprtArry = []
+
+#Loops through all files changing the name to pwprt.php
+#Also adds the pwprt to an array
+
 for filename in os.listdir("."):
 	if filename.startswith("Lab "):
 		pwprtName = filename.split('_')[1]+".php"
 		os.rename(filename, pwprtName)
 		pwprtArry.append(pwprtName.split(".")[0])
 
+#Sorts array and prints to Grades file
 pwprtArry.sort()
 for j in range (len(pwprtArry)):
     outFile.write(pwprtArry[j] + " -" + "\n")
