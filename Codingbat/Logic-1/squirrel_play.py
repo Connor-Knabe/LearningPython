0a326e1976adcea8eff090da717d6940f641f164
Connor Knabe <connorknabe@gmail.com>
2014-09-19 10:10:54 -0500

    should be working

--- Codingbat/Logic-1/squirrel_play.py
+++ Codingbat/Logic-1/squirrel_play.py
@@ -1,2 +1,6 @@
 #The squirrels in Palo Alto spend most of the day playing. In particular, they play if the temperature is between 60 and 90 (inclusive). Unless it is summer, then the upper limit is 100 instead of 90. Given an int temperature and a boolean is_summer, return True if the squirrels play and False otherwise. 
 
+	if(temp <= 100 and temp >= 60 and is_summer):
+		return True
+	elif:
+        return (temp <= 90 and temp >= 60)
