21cbc030429adbb5004b4af7520b70e197fe6562
Connor Knabe <connorknabe@gmail.com>
2014-09-21 14:02:48 -0500

    Trying a different approach

--- Codingbat/Logic-1/in1to10.py
+++ Codingbat/Logic-1/in1to10.py
@@ -1,6 +1,7 @@
 #Given a number n, return True if n is in the range 1..10, inclusive. Unless "outsideMode" is True, in which case return True if the number is less or equal to 1, or greater or equal to 10. 
 
-    if (n >= 1 and n <= 10):
-        return True
-    else:
-    	return outside_mode
+n = 1
+outside_mode = True
+
+if (n >= 1 and n <= 10 and not outside_mode):
+	print "TRUE"
