f79593c4cb711eea502fff1b813e8269c1e23128
Connor Knabe <connorknabe@gmail.com>
2014-09-22 09:46:26 -0500

    Forgot to remove the default num = 17 now all tests pass

--- Codingbat/Logic-1/near_ten.py
+++ Codingbat/Logic-1/near_ten.py
@@ -1,9 +1,5 @@
 #Given a non-negative number "num", return True if num is within 2 of a multiple of 10. Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2. See also: Introduction to Mod 
+def near_ten(num):
+    mod = num % 10
+    return (mod <= 2 or mod >= 8)
 
-num = 17
-
-mod = num % 10
-
-return (mod <= 2 or mod >= 8)
-
-print num % 10 
