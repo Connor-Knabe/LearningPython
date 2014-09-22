8282788fd42b77d5f4b288819de1c9e7cb5cb124
Connor Knabe <connorknabe@gmail.com>
2014-09-22 09:45:42 -0500

    Most tests fail

--- Codingbat/Logic-1/near_ten.py
+++ Codingbat/Logic-1/near_ten.py
@@ -1,5 +1,9 @@
 #Given a non-negative number "num", return True if num is within 2 of a multiple of 10. Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2. See also: Introduction to Mod 
 
-num = 12
+num = 17
+
+mod = num % 10
+
+return (mod <= 2 or mod >= 8)
 
 print num % 10 
