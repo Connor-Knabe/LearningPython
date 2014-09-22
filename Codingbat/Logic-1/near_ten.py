6fd1722eb8ab30387c81ffaad24cc948fc182546
Connor Knabe <connorknabe@gmail.com>
2014-09-22 09:44:12 -0500

    Working with Mod

--- Codingbat/Logic-1/near_ten.py
+++ Codingbat/Logic-1/near_ten.py
@@ -1,1 +1,5 @@
 #Given a non-negative number "num", return True if num is within 2 of a multiple of 10. Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2. See also: Introduction to Mod 
+
+num = 12
+
+print num % 10 
