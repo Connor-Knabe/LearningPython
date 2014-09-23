6e77eb88a5ab0e6153942b2edafa5f05d844e67d
Connor Knabe <connorknabe@gmail.com>
2014-09-23 08:41:16 -0500

    Logic not correct

--- Codingbat/Logic-2/lone_sum.py
+++ Codingbat/Logic-2/lone_sum.py
@@ -1,11 +1,13 @@
 #Given 3 int values, a b c, return their sum. However, if one of the values is the same as another of the values, it does not count towards the sum. 
 
-a = 2
+a = 3
 b = 2
-c = 2
+c = 3
 sum = 0
-    if (a != b and a != c):
-        sum = a+b
-    if (b != c):
-    	sum += b+c
+if (a != b and a != c):
+    sum = a+b
+if (b != c and a != c):
+	sum += b+c
+if (c != a and a != b):
+	sum += c+a	
 print sum
