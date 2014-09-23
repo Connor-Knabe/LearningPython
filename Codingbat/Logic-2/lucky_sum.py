b4d0540df020339dba47113c74b302f13ccea766
Connor Knabe <connorknabe@gmail.com>
2014-09-23 08:59:58 -0500

    All tests pass

--- Codingbat/Logic-2/lucky_sum.py
+++ Codingbat/Logic-2/lucky_sum.py
@@ -1,16 +1,18 @@
 #Given 3 int values, a b c, return their sum. However, if one of the values is 13 then it does not count towards the sum and values to its right do not count. So for example, if b is 13, then both b and c do not count. 
 
-sum=0
-a=2
-b=2
-c=2
+def lucky_sum(a, b, c):
+  
+    sum=0
 
-if a != b and a != c: sum += a
-if a == 13: print sum
-if b != a and b != c: sum += b
-if b == 13: print sum
-if c != a and c != b: sum += c
-if c == 13: print sum
+    if a == 13: return sum
+    else:
+    	sum += a
+    if b == 13: return sum
+    else:
+    	sum += b
+    if c == 13: return sum
+    else:
+    	sum += c
 
 
-print sum
+    return sum
