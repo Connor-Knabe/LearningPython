88a580bd304c38c2c564ebfb464163af76798447
Connor Knabe <connorknabe@gmail.com>
2014-09-23 08:43:50 -0500

    Another attempt

--- Codingbat/Logic-2/lone_sum.py
+++ Codingbat/Logic-2/lone_sum.py
@@ -4,10 +4,10 @@ a = 3
 b = 2
 c = 3
 sum = 0
+
 if (a != b and a != c):
-    sum = a+b
-if (b != c and a != c):
-	sum += b+c
-if (c != a and a != b):
-	sum += c+a	
-print sum
+	sum +=a
+if (b != a and b != c):
+	sum +=b
+if (c != a and c != a):
+	sum +=c
