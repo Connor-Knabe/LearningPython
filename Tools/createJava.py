c290f1d93658d82482fb7dd6420fe9dc85925f22
Connor Knabe <connorknabe@gmail.com>
2014-09-28 10:05:11 -0500

    Changed the text to say new file created

--- Tools/createJava.py
+++ Tools/createJava.py
@@ -6,10 +6,11 @@ import subprocess
 
 fName = sys.argv[1]
 if(os.path.isfile(fName+".java")):
+	
     subprocess.call("javac " + fName +".java", shell=True)
     subprocess.call("java " + fName +" ", shell=True)
 else:
-	print ("File created");
+    print ("New file created")
     file = open(fName + ".java", 'w+')
     file.write("public class " + fName + " {\n    public static void main(String[] args) {\n        \n        \n    }\n}")
     
