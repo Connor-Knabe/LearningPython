2bf98cd829e029849d1ad4a91faf86dccc06b753
Connor Knabe <connorknabe@gmail.com>
2014-09-28 09:57:13 -0500

    No variable for either subprocess call

--- Tools/createJava.py
+++ Tools/createJava.py
@@ -6,10 +6,8 @@ import subprocess
 
 fName = sys.argv[1]
 if(os.path.isfile(fName+".java")):
-    cmd = "javac " + fName +".java" 
     subprocess.call("javac " + fName +".java", shell=True)
-    cmd = "java " + fName +" "
-    subprocess.call(cmd, shell=True)
+    subprocess.call("java " + fName +" ", shell=True)
 else:
     file = open(fName + ".java", 'w+')
     file.write("public class " + fName + " {\n    public static void main(String[] args) {\n        \n        \n    }\n}")
