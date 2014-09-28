69949f081601dccda99abfbb8f92b183fb599abc
Connor Knabe <connorknabe@gmail.com>
2014-09-28 09:59:20 -0500

    Message that says new file created

--- Tools/createJava.py
+++ Tools/createJava.py
@@ -9,6 +9,7 @@ if(os.path.isfile(fName+".java")):
     subprocess.call("javac " + fName +".java", shell=True)
     subprocess.call("java " + fName +" ", shell=True)
 else:
+	print ("File created");
     file = open(fName + ".java", 'w+')
     file.write("public class " + fName + " {\n    public static void main(String[] args) {\n        \n        \n    }\n}")
     
