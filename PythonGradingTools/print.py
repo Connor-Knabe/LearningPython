8292454710fe4084d40327da6818a50272c6aca0
Connor Knabe <connorknabe@gmail.com>
2014-09-29 09:27:12 -0500

    Moving the PythonGradingTool to a different Repo

--- PythonGradingTools/print.py
+++
@@ -1,8 +0,0 @@
-import os
-arr = []
-outFile = open('./names.txt', 'w+')
-for filenames in os.listdir('.'):
-    arr.append(filenames.split(".")[0])
-arr.sort()
-for j in range (len(arr)):
-    outFile.write(arr[j] + " -" + "\n")
--- PythonGradingTools/rename.py
+++
@@ -1,4 +0,0 @@
-import os
-for filename in os.listdir("."):
-	if filename.startswith("Lab ") and "php" in filename:
-		os.rename(filename, filename.split('_')[1]+".php")
